eedf49eeaf2a935a8377877960053b9fcfff6ae8
Luke Fiorio <lukefiorio@Lukes-MBP.home>
2019-04-16 10:08:09 -1000

    completed challenge

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 function locate(array, target) {
-  // do work here
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === target) {
+      return true;
+    } else {
+      if (Array.isArray(array[i])) {
+        // put within if statement to only return true on backside if found a match up top
+        if (locate(array[i], target) === true) {
+          return true;
+        }
+      }
+    }
+  }
+  // if make it through recursive loop without finding target then return false
+  return false;
 }
 
 module.exports = locate;
